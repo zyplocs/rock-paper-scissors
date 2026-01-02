8ffe5acd3c0237b6293c06329e24cc98e7f2e440
zyplocs <zyplocked@gmail.com>
2026-01-02 16:58:50 -0600

    Touch `GameModel` and `GameRules` files

---
+++ RockPaperScissors/GameModel.swift
@@ -0,0 +1,8 @@
+//
+//  GameModel.swift
+//  RockPaperScissors
+//
+//  Created by Eli J on 1/2/26.
+//
+
+import Foundation
---
+++ RockPaperScissors/GameRules.swift
@@ -0,0 +1,8 @@
+//
+//  GameRules.swift
+//  RockPaperScissors
+//
+//  Created by Eli J on 1/2/26.
+//
+
+import Foundation
