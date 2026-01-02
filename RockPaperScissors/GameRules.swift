d5034bf0e691b573c884b262458d33626ad2d025
zyplocs <zyplocked@gmail.com>
2026-01-02 17:33:36 -0600

    Implement game logic

--- RockPaperScissors/GameRules.swift
+++ RockPaperScissors/GameRules.swift
@@ -5,4 +5,54 @@
 //  Created by Eli J on 1/2/26.
 //
 
-import Foundation
+import SwiftUI
+
+enum Move: String, CaseIterable, Identifiable {
+    case rock, paper, scissors
+    var id: String { rawValue }
+    
+    var label: String {
+        switch self {
+        case .rock: "Rock"
+        case .paper: "Paper"
+        case .scissors: "Scissors"
+        }
+    }
+    
+    /// The move this move defeats
+    var beats: Move {
+        switch self {
+        case .rock: return .scissors
+        case .paper: return .rock
+        case .scissors: return .paper
+        }
+    }
+    
+    /// The move that defeats this move
+    var losesTo: Move {
+        switch self {
+        case .rock: return .paper
+        case .paper: return .scissors
+        case .scissors: return .rock
+        }
+    }
+}
+
+enum Objective: String, CaseIterable {
+    case win, lose
+    
+    var label: String {
+        switch self {
+        case .win: "WIN"
+        case .lose: "LOSE"
+        }
+    }
+}
+
+enum Outcome { case win, lose, tie }
+
+func outcome(player: Move, versus opponent: Move) -> Outcome {
+    if player == opponent { return .tie }
+    if player.beats == opponent { return .win }
+    return .lose
+}
